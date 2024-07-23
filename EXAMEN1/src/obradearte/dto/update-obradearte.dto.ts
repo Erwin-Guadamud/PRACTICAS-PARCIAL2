80f2ec43a71b96386aad68607545b5035a72b8b9
Erwin-Guadamud <e1316066776@live.uleam.edu.ec>
2024-07-23 18:27:12 -0500

    Examen del segundo parcial NestJS

--- EXAMEN1/src/obradearte/dto/update-obradearte.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateObraDeArteDto } from './create-obradearte.dto';
-
-export class UpdateObraDeArteDto extends PartialType(CreateObraDeArteDto) {}
